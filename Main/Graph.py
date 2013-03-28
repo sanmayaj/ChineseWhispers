7bcf15be1763f34053512d820df8d33608daa21e
Sanmaya Jolly <sanmaya.jolly.cse10@itbhu.ac.in>
2013-03-28 23:01:49 +0530

    project started

---
+++ Main/Graph.py
@@ -0,0 +1,19 @@
+'''
+Created on Mar 28, 2013
+
+@author: Sanmaya Jolly
+'''
+import networkx as nx
+
+class Graph():
+    '''
+    Creates a graph using Networkx Library
+    '''
+    graph = nx.Graph()
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pass
+        
