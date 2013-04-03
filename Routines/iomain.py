6e4b7e595c22b516856197f8eb4e5d82d7f7a156
Sanmaya Jolly <sanmaya.jolly.cse10@itbhu.ac.in>
2013-04-03 18:33:14 +0530

    Main file added

---
+++ Routines/iomain.py
@@ -0,0 +1,25 @@
+'''
+Created on Apr 3, 2013
+
+@author: Sanmaya Jolly
+'''
+
+from graph import Graph
+from cluster import Cluster
+
+if __name__ == '__main__':
+    n = raw_input()
+    g = Graph()
+    lclusters = []
+    for i in range(n):
+        l = raw_input().split()
+        length = len(l)
+        node_name = l[0]
+        #node_edges = 
+        node_nbrs = l[1:]
+        g.create_node(node_name)
+        g.graf.add_weighted_edges_from((node_name, nbr, 1) for nbr in node_nbrs)
+        c = Cluster(ID = g.graf.node[node_name]['label'])
+        c.add_node(node_name)
+        #c.add_node(g.graf.node[node_name]['label'])
+        lclusters.append(c)
