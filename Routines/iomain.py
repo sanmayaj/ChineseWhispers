79ae628590a52652260feb63a33fbc8b7cb90439
Sanmaya Jolly <sanmaya.jolly.cse10@itbhu.ac.in>
2013-04-10 00:34:57 +0530

    New main for cluster creation and updation, corrected 'New Cluster' bug

--- Routines/iomain.py
+++ Routines/iomain.py
@@ -7,30 +7,33 @@ Created on Apr 3, 2013
 from graph import Graph
 from cluster import Cluster
 from graphmanip import *
+from clustermanip import random_cluster
 from ioaux import file_input_test_data as input_from_file
 
 if __name__ == '__main__':
-    '''n = raw_input()
-    g = Graph()
-    lclusters = []
-    for i in range(n):
-        l = raw_input().split()
-        length = len(l)
-        node_name = l[0]
-        #node_edges = 
-        node_nbrs = l[1:]
-        g.create_node(node_name)
-        g.graf.add_weighted_edges_from((node_name, nbr, 1) for nbr in node_nbrs)
-        c = Cluster(ID = g.graf.node[node_name]['CLID'])
-        c.add_node(node_name)
-        #c.add_node(g.graf.node[node_name]['label'])
-        lclusters.append(c)'''
     g = Graph()
     g = input_from_file()
     clustersli = []
-    for node_name in g.nodes():
-        c = Cluster(ID = get_max_weighted_clusters(g, node_name))
-        c.add_node(node_name)
-        change_node_cluster(g, node_name, c.label)
-        clustersli.append(c)
+    for node_name in g.graf.nodes():
+        newclusters = get_max_weighted_clusters(g, node_name)
+        newcluster_name = random_cluster(newclusters)
+        #print "Node:", node_name, "Newcluster:", newcluster_name
+        if newcluster_name:
+            found = False
+            for c in clustersli:
+                if c.label == newcluster_name:
+                    c.add_node(node_name)
+                    found = True
+                    break
+            if not found:
+                c = Cluster(ID = newcluster_name)
+                c.add_node(node_name)
+                clustersli.append(c)
+            change_node_cluster(g, node_name, c.label)
+        else:
+            c = Cluster(ID = node_name)
+            c.add_node(node_name)
+            clustersli.append(c)
+    for c in clustersli:
+        print c.label, c.node_list
     
