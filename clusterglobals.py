713bc3bde3556373e4917553f629d182665939dc
Sanmaya Jolly <sanmaya.jolly.cse10@itbhu.ac.in>
2013-06-02 12:08:34 +0530

    cluster retrieval method added

--- clusterglobals.py
+++ clusterglobals.py
@@ -4,3 +4,10 @@ Created on Jun 1, 2013
 @author: Sanmaya Jolly
 '''
 clusters_list = []
+
+def get_cluster(cluster_id):
+    global clusters_list
+    for cluster in clusters_list:
+        if cluster.label == cluster_id:
+            return cluster
+    return None
